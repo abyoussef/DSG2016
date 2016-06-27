e1d4c02abf51b9378d5de9d371ef0c6d8871f4f3
marioyc <ycmario@gmail.com>
2016-06-27 17:29:17 +0200

    add 2d diagram of sizes

--- image_sizes.py
+++ image_sizes.py
@@ -9,33 +9,35 @@ with open('id_train.csv', 'rb') as csvfile:
 	data = csv.DictReader(csvfile)
 
 	cont_label = {1 : 0, 2 : 0, 3 : 0, 4 : 0}
-	hlist = {}
-	wlist = {}
+	hcont = {}
+	wcont = {}
+	hlist = []
+	wlist = []
 
 	for row in data:
 		id = row['Id']
 		label = int(row['label'])
 		img = cv2.imread('roof_images/' + id + '.jpg')
 
-		if img.shape in cont_size:
-			cont_size[img.shape] += 1
-		else:
-			cont_size[img.shape] = 1
 		cont_label[label] += 1
 
 		h, w = img.shape[0] / 10, img.shape[1] / 10
-		if h in hlist:
-			hlist[h] += 1
+		hlist.append(h)
+		wlist.append(w)
+		if h in hcont:
+			hcont[h] += 1
 		else:
-			hlist[h] = 1
+			hcont[h] = 1
 
-		if w in wlist:
-			wlist[w] += 1
+		if w in wcont:
+			wcont[w] += 1
 		else:
-			wlist[w] = 1
+			wcont[w] = 1
 
 	print cont_label
-	pandas.Series(hlist).plot(kind='bar', title='height')
+	plt.scatter(hlist, wlist)
 	plt.show()
-	pandas.Series(wlist).plot(kind='bar', title='width')
+	pandas.Series(hcont).plot(kind='bar', title='height')
+	plt.show()
+	pandas.Series(wcont).plot(kind='bar', title='width')
 	plt.show()
