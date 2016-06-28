298027bb7260e13b3e901d0572f8f4ee9d4e7a91
marioyc <ycmario@gmail.com>
2016-06-28 15:30:49 +0200

    rewrite submission comparison in python

---
+++ compare_submissions.py
@@ -0,0 +1,43 @@
+import csv
+import cv2
+
+submission_name1 = 'submission9'
+submission_name2 = 'submission10'
+detailed = False
+limit = 10
+
+if detailed:
+	file1 = submission_name1 + '_detailed.csv'
+	file2 = submission_name2 + '_detailed.csv'
+else:
+	file1 = submission_name1 + '.csv'
+	file2 = submission_name2 + '.csv'
+
+with open(file1, 'rb') as fsub1:
+	with open(file2, 'rb') as fsub2:
+		sub1 = csv.DictReader(fsub1)
+		sub2 = csv.DictReader(fsub2)
+		data1 = []
+		data2 = []
+		for row in sub1:
+			data1.append(row)
+		for row in sub2:
+			data2.append(row)
+		
+		assert(len(data1) == len(data2))
+		len = len(data1)
+
+		for i in range(0,len):
+			cur1, cur2 = data1[i], data2[i]
+			assert(cur1['Id'] == cur2['Id'])
+			id = cur1['Id']
+
+			if cur1['label'] != cur2['label'] and limit > 0:
+				i = cv2.imread('roof_images/' + id + '.jpg')
+				print 'sub1 =', cur1['label'], 'sub2 =', cur2['label']
+				if detailed:
+					print(cur1['cat1'], cur1['cat2'], cur1['cat3'], cur1['cat4'])
+					print(cur2['cat1'], cur2['cat2'], cur2['cat3'], cur2['cat4'])
+				cv2.imshow('img', i)
+				cv2.waitKey(0)
+				limit -= 1
