377e58d194a2515338207e8143773c8d1f1178b0
Mario Ynocente Castro <ycmario@gmail.com>
2016-06-29 00:56:58 +0200

    add loop

--- run_exp.sh
+++ run_exp.sh
@@ -1,2 +1,7 @@
 th main.lua -modelName model0 -preprocess
 th test_net.lua -modelName model0 -submissionName submission0 -preprocess
+
+#for i in {1..3}
+#do
+#   th main.lua -modelName model$i -augmentedData
+#done
