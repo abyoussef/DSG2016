e5c009b0111498dda2f5e2a04b393c8f53c99437
Mario Ynocente Castro <ycmario@gmail.com>
2016-07-03 14:43:32 +0200

    count labels that stayed the same

--- compare_submissions.py
+++ compare_submissions.py
@@ -27,9 +27,9 @@ with open(file1, 'rb') as fsub1:
 
 		assert(len(data1) == len(data2))
 		len = len(data1)
-        discrepance_matrix = []
+        comparison_matrix = []
         for i in range(0,classes):
-            discrepance_matrix.append([0] * classes)
+            comparison_matrix.append([0] * classes)
         cont_discrepancies = 0
 
         for i in range(0,len):
@@ -37,19 +37,18 @@ with open(file1, 'rb') as fsub1:
             assert(cur1['Id'] == cur2['Id'])
             id = cur1['Id']
 
-            if cur1['label'] != cur2['label']:
-                discrepance_matrix[ int(cur1['label']) - 1 ][ int(cur2['label']) - 1 ] += 1
+            comparison_matrix[ int(cur1['label']) - 1 ][ int(cur2['label']) - 1 ] += 1
 
             if cur1['label'] != cur2['label']:
+                cont_discrepancies += 1
                 if detailed:
                     print cur1['Id']
                     print cur1['label'], "(" + cur1['cat' + cur1['label']] +  ")", \
                             cur1['cat1'], cur1['cat2'], cur1['cat3'], cur1['cat4']
                     print cur2['label'], "(" + cur2['cat' + cur2['label']] +  ")", \
                             cur2['cat1'], cur2['cat2'], cur2['cat3'], cur2['cat4']
-                    cont_discrepancies += 1
                 if limit > 0:
-                    print 'sub1 =', cur1['label'], 'sub2 =', cur2['label']
+                    print 'Id = ', cur1['Id'] ,'sub1 =', cur1['label'], 'sub2 =', cur2['label']
                     i = cv2.imread('roof_images/' + id + '.jpg')
                     cv2.imshow('img', i)
                     cv2.waitKey(0)
@@ -57,4 +56,4 @@ with open(file1, 'rb') as fsub1:
 
         print cont_discrepancies
         for i in range(0,classes):
-            print(discrepance_matrix[i])
+            print(comparison_matrix[i])
